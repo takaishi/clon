f901dab8f8304551087543dda3baf20b6ef51871
r_takaishi <ryo.takaishi.0@gmail.com>
2018-10-27 15:45:45 +0900

    support skp to job if japanese holiday

--- job/job.go
+++ job/job.go
@@ -1,24 +1,42 @@
 package job
 
 import (
+	jp "github.com/kokardy/jpholiday"
 	"log"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 type Job struct {
-	Name     string `yaml:"name"`
-	Schedule string `yaml:"schedule"`
-	Command  string `yaml:"command"`
+	Name     string  `yaml:"name"`
+	Schedule string  `yaml:"schedule"`
+	Command  string  `yaml:"command"`
+	Options  Options `yaml:"options"`
+}
+
+type Options struct {
+	SkipJPHoliday bool `yaml:"skip_jp_holiday"`
 }
 
 func (j Job) Run() {
-	cmd := strings.Split(j.Command, " ")
-	out, err := exec.Command(cmd[0], cmd[1:]...).Output()
-	if err != nil {
-		log.Printf("[ERROR] Failed to exec: %s\n", err)
+	now := time.Now()
+	if j.isJPHoliday(now) {
+		log.Printf("[INFO] Skip to execute job: %s/%s/%s is Japanese holiday.", now.Year(), now.Month(), now.Day())
 	} else {
-		log.Printf("[INFO] %s\n", out)
-		log.Printf("[INFO] Success")
+		cmd := strings.Split(j.Command, " ")
+		out, err := exec.Command(cmd[0], cmd[1:]...).Output()
+		if err != nil {
+			log.Printf("[ERROR] Failed to exec: %s\n", err)
+		} else {
+			log.Printf("[INFO] %s\n", out)
+			log.Printf("[INFO] Success")
+		}
 	}
+}
+
+func (j Job) isJPHoliday(t time.Time) bool {
+	day := jp.NewDate(t.Year(), t.Month(), t.Day())
+	isJPHoliday, _ := day.Holiday()
+	return isJPHoliday
 }
